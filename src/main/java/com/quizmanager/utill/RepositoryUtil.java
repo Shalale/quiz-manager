41afac229df20ec265c1db9ad82b5763a5cd9f86
Shalale <selale.memmedli11@gmail.com>
2024-09-22 19:42:34 +0400

    implementing exception handler

--- src/main/java/com/quizmanager/utill/RepositoryUtil.java
+++ src/main/java/com/quizmanager/utill/RepositoryUtil.java
@@ -1,11 +1,12 @@
 package com.quizmanager.utill;
 
+import com.quizmanager.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class RepositoryUtil {
     public static <T, ID> T fetchById(JpaRepository<T, ID> repository, ID id){
         return repository.findById(id).orElseThrow(
-                () -> new RuntimeException("Entity not found with id: " + id)
+                () -> new NotFoundException("Entity not found with id: " + id)
         );
     }
 }
